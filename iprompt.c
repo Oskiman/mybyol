546b60b8cd910d35c1b2b4b5749ff48e54fd0c94
Paul <Oskiman@users.noreply.github.com>
2025-05-09 12:16:26 +0100

    Add files via upload

---
+++ iprompt.c
@@ -0,0 +1,27 @@
+// code from byol website
+#include <stdio.h>
+
+// declare a buffer for user input of size 2048
+static char input[2048];
+
+int main(int argc, char** argv)
+{
+	// print version and exit information
+	puts("Lispy Version 0.0.0.0.1");
+	puts("Press Ctrl+c to exit\n");
+
+	//in a never ending loop
+	while(1)
+	{
+		// output prompt
+		fputs("lispy> ", stdout);
+
+		// read a line of user input of max size 2048
+		fgets(input, 2048, stdin);
+
+		// echo input back to user
+		printf("No, you're a %s", input);
+	}
+
+	return 0;
+}
